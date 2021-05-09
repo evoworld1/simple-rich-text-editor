b64e77468cc1b369a1a98fdde48aea02108eb156
evoworld1 <evoworld1@gmail.com>
2021-05-09 23:51:46 +0200

    Add local storage save/load options

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 let buttons = document.querySelectorAll(".toolbar-btn");
+
 for (let button of buttons) {
   button.addEventListener("click", () => {
-    let cmd = button.dataset["command"];
-    document.execCommand(cmd, false, null);
+    let setCommand = button.dataset["command"];
+    document.execCommand(setCommand, false, null);
   });
 }
 
+//Export JSON file
+
 const exportJson = () => {
   let outputToSave = document.querySelector(".editable-text").innerHTML;
-  const data = { content: outputToSave };
+  const data = { outputToSave };
   const a = document.createElement("a");
   a.href = URL.createObjectURL(
     new Blob([JSON.stringify(data, null, 2)], {
@@ -19,4 +22,21 @@ const exportJson = () => {
   document.body.appendChild(a);
   a.click();
   document.body.removeChild(a);
+};
+
+// Local Storage
+
+const populateStorage = () => {
+  let outputToSave = document.querySelector(".editable-text").innerHTML;
+  const data = { outputToSave };
+  localStorage.clear();
+  localStorage.setItem("savedOutput", JSON.stringify(data));
+};
+
+const loadStorage = () => {
+  let output = document.querySelector(".editable-text");
+  let savedOutput = localStorage.getItem("savedOutput");
+  parsedSavedOutput = JSON.parse(savedOutput);
+  outputToSave = parsedSavedOutput;
+  output.innerHTML = outputToSave.outputToSave;
 };
